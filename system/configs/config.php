a6d4b9b837e10bd04f8e831b5e3a012bffbe4882
D0Gmatist <375256232834@yandex.by>
2016-07-19 20:47:32 +0300

    Add files via upload

---
+++ system/configs/config.php
@@ -0,0 +1,50 @@
+<?PHP 
+
+//System Configurations
+
+$config = array (
+	'http_home_url' => 'http://magazinx.by/',
+
+	'skin' 	=> 'catharsis',
+
+	'home_title' => 'CATHARSIS SHOP', // заголовок главной
+
+	'description' => 'CATHARSIS SHOP - CMS движок',
+
+	'keywords' => 'catharsis, shop, движок',
+
+	'spidbar_title' => 'Главная',
+
+	'charset' => 'utf-8',
+
+	'allow_cache' => 'yes',
+
+	'clear_cache' => '120',
+
+	'nav_number' => '10',
+
+	'nav_reviews_number' => '10',
+
+	'type_navigation' => '1',
+
+	'tupe_url' => '1',
+
+	'langs' => 'ru',
+
+	'email' => '375256232834@yandex.by',
+
+	'auth_domain' => '1',
+
+	'separator' => '&raquo;',
+
+	'img_size' => '10',
+
+	'img_wh' => '400',
+
+	'file_size' => '200',
+
+	'img_ext' => 'jpg,jpeg,gif,png',
+
+);
+
+?>
---
+++ system/configs/dbconfig.php
@@ -0,0 +1,17 @@
+<?PHP
+
+define ("DBHOST", "localhost");
+
+define ("DBNAME", "magazin");
+
+define ("DBUSER", "magazin");
+
+define ("DBPASS", "magazin");
+
+define ("PREFIX", "CATHARSIS");
+
+define ("COLLATE", "utf8");
+
+$db = new db;
+
+?>
